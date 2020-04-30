30a5eca41d28cc94b1cd21770ea399749129e6b3
Jeremiah_J_Thomas <jt6fz@virginia.edu>
2020-04-29 22:04:47 -0400

    files for simple dvd chamber

--- stats.py
+++ stats.py
@@ -3,9 +3,9 @@
 
 import numpy as np
 d = dump("part.vent")
-d.tselect.one(320000)
-d.aselect.all(320000)
-vx,vy = d.vecs(320000, "vx", "vy")
+d.tselect.one(325000)
+d.aselect.all(325000)
+vx,vy = d.vecs(325000, "vx", "vy")
 vent_vx = np.average(vx)
 vent_vy = np.average(vy)
 # print "average vx in the vent:", vent_vx
@@ -14,15 +14,25 @@ vent_vy = np.average(vy)
 #                                         np.square(vent_vy))
 
 d = dump("part.downstream")
-d.tselect.one(320000)
-d.aselect.all(320000)
-vx,vy = d.vecs(320000, "vx", "vy")
+d.tselect.one(325000)
+d.aselect.all(325000)
+vx,vy = d.vecs(325000, "vx", "vy")
 downstream_vx = np.average(vx)
 downstream_vy = np.average(vy)
 # print "average vx downstream:", downstream_vx
 # print "average vy downstream:", downstream_vy
 # print "average v downstream:", np.sqrt(np.square(downstream_vx) +
 #                                        np.square(downstream_vy))
+
+
+d = dump("part.final")
+d.tselect.one(325000)
+d.aselect.all(325000)
+d.aselect.test("$x > -.045 and $x < -.015 and $y < .09 and $y < 0.09",325000)
+vx,vy = d.vecs(325000, "vx", "vy")
+expansion_vx = np.average(vx)
+expansion_vy = np.average(vy)
+
 
 d = dump("press.final")
 d.tselect.one(325000)
@@ -49,6 +59,11 @@ print "average vx downstream:", downstream_vx
 print "average vy downstream:", downstream_vy
 print "average v downstream:", np.sqrt(np.square(downstream_vx) +
                                        np.square(downstream_vy))
+
+print "average vx expansion:", expansion_vx
+print "average vy expansion:", expansion_vy
+print "average v expansion:", np.sqrt(np.square(expansion_vx) +
+                                       np.square(expansion_vy))
+
 print "average pressure upstream:", upstream_p
 print "average pressure downstream:", downstream_p
-exit()
