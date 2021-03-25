48ff1ca4c2cf2b710036b3fdaeaaa386afe77020
xivh <jt6fz@virginia.edu>
2021-03-25 18:45:26 -0400

    pressure boundary conditions

--- dvd.sh
+++ dvd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=20
-#SBATCH --time=0:30:00
+#SBATCH --time=5:00:00
 #SBATCH --output=out.txt
-#SBATCH --partition=parallel
+#SBATCH --partition=standard
 #SBATCH -A wadleygroup
 
 srun ../spa_mpi < in.dvd
