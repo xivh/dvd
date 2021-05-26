6d404b3c5762d1655692568f710d9ab1bbd8d142
xivh <jt6fz@virginia.edu>
2021-05-26 17:21:07 -0400

    adapting grid cells - unstable

--- dvd.sh
+++ dvd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=20
-#SBATCH --time=5:00:00
+#SBATCH --time=1:00:00
 #SBATCH --output=out.txt
 #SBATCH --partition=standard
 #SBATCH -A wadleygroup
