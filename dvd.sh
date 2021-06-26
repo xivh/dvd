790ecef635cc9db06687f45173bff32ea1145ad1
xivh <jt6fz@virginia.edu>
2021-06-26 19:10:45 -0400

    sparta upload

--- dvd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=20
-#SBATCH --time=0:30:00
-#SBATCH --output=out.txt
-#SBATCH --partition=parallel
-#SBATCH -A wadleygroup
-
-srun ../spa_mpi < speedtest.dvd
